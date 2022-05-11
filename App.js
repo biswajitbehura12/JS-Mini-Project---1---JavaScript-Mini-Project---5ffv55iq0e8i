f9f17eadd10e6b5d84f62900c439797765902a56
BISWAJIT BEHURA <102812316+biswajitbehura12@users.noreply.github.com>
2022-05-11 22:43:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-
+ function login(){
+         var unname=document.getElementById("userid").value;
+         var pass=document.getElementById("password").value;
+         if(unname =="biswajitbehura12"&& pass =="babul@567")
+         {
+             location.assign("file:///C:/Users/User/Desktop/login%20page/babul.html");
+         }else{
+             window.alert("login failed");
+         }
+     }
